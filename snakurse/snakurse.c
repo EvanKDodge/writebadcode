96fe3e5aa9a750f797ff9bc8d9b95dd7be666806
HaxQ <kninja_pi@yahoo.com>
2024-10-12 15:55:28 -0700

    snake: basic control, rot13: add test files

--- snakurse/snakurse.c
+++ snakurse/snakurse.c
@@ -1,52 +1,86 @@
 #include <curses.h>
 #include <unistd.h>
 
-int kbhit(void)
-{
-    int ch = getch();
+typedef struct Player {
+	int r;
+	int c;
+	int rvel;
+	int cvel;
+} Player;
 
-    if (ch != ERR) {
-        ungetch(ch);
-        return 1;
-    } else {
-        return 0;
-    }
-}
+int screen_init();
+Player player_init();
+void player_dir_change(Player*, int, int);
+void player_update(Player*);
 
 int main(void)
 {
-	int r, c, rvel, cvel;
+	char c;
+	Player user;
 
-	//curses initialization
-	initscr();
-	cbreak();
-	noecho();
-	nodelay(stdscr, TRUE);
-	curs_set(0);
+	screen_init();
+	user = player_init();
 
-	//init ball
-	r = LINES / 2;
-	c = COLS / 2;
-	rvel = 1;
-	cvel = 1;
-
-	while(!kbhit())
+	while((c = getch()) != 'q')
 	{
-		if(c >= COLS - 1 || c <= 0)
-		{
-			cvel = -cvel;
-		}
-		if(r >= LINES - 1 || r <= 0)
-		{
-			rvel = -rvel;
-		}
 		erase();
-		mvaddch(r, c, 'O');
-		c += cvel;
-		r += rvel;
+		mvaddch(user.r, user.c, '@');
+		switch(c)
+		{
+			case 'w':
+				player_dir_change(&user, -1, 0);
+				break;
+			case 's':
+				player_dir_change(&user, 1, 0);
+				break;
+			case 'a':
+				player_dir_change(&user, 0, -1);
+				break;
+			case 'd':
+				player_dir_change(&user, 0, 1);
+				break;
+		};
+		player_update(&user);
+		usleep(150000);
 		refresh();
-		usleep(30000);
 	}
 
 	endwin();
+
+	return 0;
+}
+
+int screen_init()
+{
+	initscr(); //init curses
+	cbreak(); //no buffering...make characters immediately available
+	noecho(); //don't show characters the user has typed
+	nodelay(stdscr, TRUE); //make getch() a non-blocking call
+	curs_set(0); //make cursor invisible (for now anyway)
+
+	return 0;
+}
+
+void player_dir_change(Player *p, int rvel, int cvel)
+{
+	p->rvel = rvel;
+	p->cvel = cvel;
+}
+
+void player_update(Player *p)
+{
+	p->r += p->rvel;
+	p->c += p->cvel;
+}
+
+Player player_init()
+{
+	Player p;
+
+	p.r = LINES / 2;
+	p.c = LINES / 2;
+	p.rvel = 0;
+	p.cvel = 0;
+
+	return p;
 }
