6edd68a5ce1157a3d000df8e743743dc63d63b5f
HaxQ <kninja_pi@yahoo.com>
2024-10-14 12:05:33 -0700

    Making snakurse more modulear. Added TODO

--- snakurse/snakurse.c
+++ snakurse/snakurse.c
@@ -1,9 +1,13 @@
 #include <curses.h>
 #include <unistd.h>
 
-typedef struct Player {
+typedef struct Segment {
 	int r;
 	int c;
+} Segment;
+
+typedef struct Player {
+	Segment body[1];
 	int rvel;
 	int cvel;
 	int alive;
@@ -13,6 +17,8 @@ int screen_init();
 Player player_init();
 void player_dir_change(Player*, int, int);
 void player_update(Player*);
+void draw(Player*);
+void player_input(Player*, char);
 
 int main(void)
 {
@@ -24,46 +30,17 @@ int main(void)
 
 	while((c = getch()) != 'q')
 	{
-		erase();
+		erase(); //clear the screen
+
+		player_input(&user, c);
 		if(user.alive)
 		{
-			mvaddch(user.r, user.c, '@');
-			if(user.r < 0 || user.r > (LINES - 1) || user.c < 0 || user.c > (COLS - 1))
-			{
-				user.alive = 0;
-			}
-			switch(c)
-			{
-				case 'w':
-					if(user.rvel != 1)
-						player_dir_change(&user, -1, 0);
-					break;
-				case 's':
-					if(user.rvel != -1)
-						player_dir_change(&user, 1, 0);
-					break;
-				case 'a':
-					if(user.cvel != 1)
-						player_dir_change(&user, 0, -1);
-					break;
-				case 'd':
-					if(user.cvel != -1)
-						player_dir_change(&user, 0, 1);
-					break;
-			};
 			player_update(&user);
 			usleep(100000);
 		}
-		else
-		{
-			mvaddstr(LINES / 2, COLS / 2 - 4, "Game Over");
-			mvaddstr(LINES / 2 + 2, COLS / 2 - 9, "Press 'r' to reset");
-			if(c == 'r')
-			{
-				user = player_init();
-			}
-		}
-		refresh();
+
+		draw(&user); //draw what needs drawing
+		refresh();   //refresh the screen
 	}
 
 	endwin();
@@ -90,16 +67,67 @@ void player_dir_change(Player *p, int rvel, int cvel)
 
 void player_update(Player *p)
 {
-	p->r += p->rvel;
-	p->c += p->cvel;
+	if(p->body[0].r < 0 || p->body[0].r > (LINES - 1) || p->body[0].c < 0 || p->body[0].c > (COLS - 1))
+	{
+		p->alive = 0;
+	}
+
+	p->body[0].r += p->rvel;
+	p->body[0].c += p->cvel;
+}
+
+void draw(Player *p)
+{
+	if(p->alive)
+	{
+		mvaddch(p->body[0].r, p->body[0].c, '@');
+	}
+	else
+	{
+		mvaddstr(LINES / 2, COLS / 2 - 4, "Game Over");
+		mvaddstr(LINES / 2 + 2, COLS / 2 - 9, "Press 'r' to reset");
+	}
+}
+
+void player_input(Player *p, char c)
+{
+	if(p->alive)
+	{
+		switch(c)
+		{
+			case 'w':
+				if(p->rvel != 1)
+					player_dir_change(p, -1, 0);
+				break;
+			case 's':
+				if(p->rvel != -1)
+					player_dir_change(p, 1, 0);
+				break;
+			case 'a':
+				if(p->cvel != 1)
+					player_dir_change(p, 0, -1);
+				break;
+			case 'd':
+				if(p->cvel != -1)
+					player_dir_change(p, 0, 1);
+				break;
+		};
+	}
+	else
+	{
+		if(c == 'r')
+		{
+			*p = player_init();
+		}
+	}
 }
 
 Player player_init()
 {
 	Player p;
 
-	p.r = LINES / 2;
-	p.c = LINES / 2;
+	p.body[0].r = LINES / 2;
+	p.body[0].c = COLS / 2;
 	p.rvel = 0;
 	p.cvel = 1;
 	p.alive = 1;
